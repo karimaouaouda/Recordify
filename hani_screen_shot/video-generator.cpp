343ad03146613548051c0c2837a35299ac8a604c
Hani <145288127+elbos12@users.noreply.github.com>
2025-11-19 11:09:20 -1100

    code to process 100 image and convert it to video

--- hani_screen_shot/video-generator.cpp
+++ hani_screen_shot/video-generator.cpp
@@ -4,12 +4,12 @@
 #include <iostream>
 #include <codecvt>
 #include <windows.h>
-// #include <fstream>
 #include <vector>
 using namespace std;
 cv::Mat readImage(int number);
 // define size of out images frame
-int frame_width, frame_height;
+int frame_width = 1920;
+int frame_height = 1080;
 
 // define class of write video
 cv::VideoWriter writeVideo;
@@ -17,26 +17,25 @@ cv::VideoWriter writeVideo;
 cv::Mat image;
 int main()
 {
-    cout << "start with modified code" << endl;
     // define the video codec used in the video
-    int videoCodec = writeVideo.fourcc('X', 'V', 'I', 'D');
-
+    int videoCodec = writeVideo.fourcc('M', 'J', 'P', 'G');
     // define resolution of video from the frames
     cv::Size resolution = cv::Size(frame_width, frame_height);
 
     // Define the codec and create VideoWriter object with informations about our video
-    cv::VideoWriter out("output.avi", videoCodec, 30.0, resolution);
+    cv::VideoWriter out("output.mp4", videoCodec, 30, cv::Size(frame_width, frame_height));
 
-    // check if is opened
+    cout << "directly after making the video write function: " << GetLastError() << endl;
+    // check if is video writer is opened
     if (!out.isOpened())
     {
         std::cerr << "Error: Could not open the video writer." << std::endl;
 
         cout << "the error is: " << GetLastError() << endl;
-        return -1; // Or handle the error appropriately
+        return -1;
     }
     // lop the images frames
-    for (int number; number <= 100; number++)
+    for (int number = 0; number <= 200; number++)
     {
         cv::Mat image = readImage(number);
 
@@ -52,11 +51,6 @@ int main()
         // Display the frame
     }
     out.release();
-    // cv::imshow("Frame", image);
-    // if (cv::waitKey(1) == 'q')
-    // {
-    //     break;
-    // }
     return 0;
 }
 
@@ -64,57 +58,26 @@ cv::Mat readImage(int number)
 {
     // resized image
     cv::Mat resizedImage;
+
     string imagepath;
 
-    cout << "before start read the image" << endl;
     wstring imageName = L"image_" + to_wstring(number) + L".jpg";
-    // C:\python_train\python_downloader\downloaded_images
-    wstring imagepathC = L"C:\\python_train\\python_downloader\\downloaded_images\\" + imageName;
+
+    // the path of out images(frames)
+    wstring imagepathC = L"C:\\" + imageName;
+
+    // object used to convert between wstring and string UTF-8
     std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+
+    // convert our variable from wstring to string(UTF-8)
     imagepath = converter.to_bytes(imagepathC);
+
+    // read the image from that path with color
     cv::Mat image = cv::imread(imagepath, cv::IMREAD_COLOR);
+
+    // resize our image with speicifique width and height without any Distorted
     cv::resize(image, resizedImage, cv::Size(frame_width, frame_height), 0, 0, cv::INTER_LINEAR);
-    cout << "after read the image" << endl;
-    // Display the image in a window
-    // cv::imshow("Displayed Image", image);
 
-    // Wait for a key press before closing the window
-    // cv::waitKey(0);
-    // cv::destroyAllWindows();
-    // Open the file in binary input mode
-    // std::ifstream file(filename, std::ios::in | std::ios::binary);
-
-    // if (!file.is_open())
-    // {
-    //     std::cerr << "Error opening file: " << filename << std::endl;
-    //     return 1;
-    // }
-
-    // // Seek to the end of the file to determine its size
-    // file.seekg(0, std::ios::end);
-    // std::streampos fileSize = file.tellg();
-    // file.seekg(0, std::ios::beg); // Seek back to the beginning
-
-    // // Create a vector to store the image data
-    // std::vector<char> imageData(fileSize);
-
-    // // Read the entire file into the vector
-    // file.read(imageData.data(), fileSize);
-
-    // // Check for read errors
-    // if (!file)
-    // {
-    //     std::cerr << "Error reading file: " << filename << std::endl;
-    //     return 1;
-    // }
-
-    // file.close(); // Close the file
-
-    // std::cout << "Successfully read " << fileSize << " bytes from " << filename << std::endl;
-    // number += 1;
+    // return the resized image
     return resizedImage;
-    // }
-
-    // The 'imageData' vector now contains the raw binary data of the image.
-    // You can then process this data, for example, by passing it to an image processing library.
 }
